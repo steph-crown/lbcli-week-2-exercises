d64a1abd588343af45807ba2331878b783b04dbf
Steph Crown <emmanuelstephen024@gmail.com>
2026-07-01 19:35:31 +0100

    Add solutions for 01 to 05.sh

--- submission/02.sh
+++ submission/02.sh
@@ -1,1 +1,3 @@
 # Create a new Bitcoin address, for receiving change.
+bitcoin-cli -regtest getrawchangeaddress
+# bcrt1qszhjp542t4tph09qrkfmzeqpph9tnek6c0xysg
--- submission/03.sh
+++ submission/03.sh
@@ -1,3 +1,8 @@
 # Create a SegWit address.
 # Add funds to the address.
 # Return only the Address
+SEGWIT_ADDRESS=$(bitcoin-cli -regtest getnewaddress "segwit" "p2sh-segwit")
+
+MUTE=$(bitcoin-cli -regtest generatetoaddress 101 "$SEGWIT_ADDRESS")
+
+echo "$SEGWIT_ADDRESS"
